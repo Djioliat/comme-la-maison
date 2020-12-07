8536a309ae23aac7d9ac56451cb51dcb53232a23
ysp <54001533+ysp313@users.noreply.github.com>
2020-12-07 11:45:37 +0100

    modif class FOOD et WINE
    ajout de description image et migrations

---
+++ migrations/Version20201207104125.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201207104125 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE food ADD image_description VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE wine ADD image_description VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE food DROP image_description');
+        $this->addSql('ALTER TABLE wine DROP image_description');
+    }
+}
