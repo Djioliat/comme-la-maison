e66c171035bd4cd5aa0877f1112c7b3ee85ca080
ysp <54001533+ysp313@users.noreply.github.com>
2020-12-07 10:23:23 +0100

    Update FoodType.php
    Ajout des type dans le formulaire des plats

--- src/Form/FoodType.php
+++ src/Form/FoodType.php
@@ -4,7 +4,11 @@ namespace App\Form;
 
 use App\Entity\Food;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,16 +17,23 @@ class FoodType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('type')
-            ->add('description')
+            ->add('name', TextType::class)
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Entrée' => 'entree',
+                    'Plat' => 'plat',
+                    'Dessert' => 'dessert',
+                    'Fromage' => 'fromage',
+                    'Végétarien' => 'vegetarien'
+                ],
+            ])
+            ->add('description', TextareaType::class)
             ->add('picture', FileType::class, [
                 'mapped' => false,
                 'required' => false
-
             ])
-            ->add('priceRestaurant')
-            ->add('priceTakeway')
+            ->add('priceRestaurant', NumberType::class)
+            ->add('priceTakeway', NumberType::class)
         ;
     }
 
