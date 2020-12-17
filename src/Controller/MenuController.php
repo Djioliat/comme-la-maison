f3b0b67873b9d50342d062f686c12a9f2101b43f
Djioliat <alverbreugh@gmail.com>
2020-12-17 17:04:49 +0100

    page menu

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Menu;
+use App\Entity\Food;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,12 +14,12 @@ class MenuController extends AbstractController
      */
     public function index(): Response
     {
-        $menus = $this->getDoctrine()
-             ->getRepository(Menu::class)
+        $foods = $this->getDoctrine()
+             ->getRepository(Food::class)
              ->findAll();
 
         return $this->render('menu/index.html.twig', [
-            'menus' => $menus
+            'foods' => $foods
         ]);
     }
 }
