287a1d291bf05f6678fe2402df6e4e577e3f412a
ysp <54001533+ysp313@users.noreply.github.com>
2020-12-22 09:54:39 +0100

    ajout
    Ajout des listes de plats et de vins

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\WineRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\FoodRepository;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
     /**
      * @Route("/", name="homepage")
      */
-    public function home() {
-        return $this->render('home.html.twig');
+    public function home(FoodRepository $foodRepository, WineRepository $wineRepository) {
+        return $this->render('home.html.twig', [
+            'foods' => $foodRepository->findAll(),
+            'wines' => $wineRepository->findAll()
+        ]);
     }
     /**
      * @Route("/admin", name="adminpage")
