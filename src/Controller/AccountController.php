2b502e1b25e7a09b066029ef6dadf46ae4530ddc
Djioliat <alverbreugh@gmail.com>
2020-12-24 06:54:11 +0100

    inscription users

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/login", name="account_login")
+     */
+    public function login(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+
+
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null
+        ]);
+    }
+
+    /**
+     * Permet de se déconnecter
+     * 
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout(){
+        // ....rien
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'inscription
+     * 
+     * @Route("/register", name="account_register")
+     */
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder) {
+        
+        
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $password = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password); 
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Votre compte a bien été créé, vous pouvez vous connecter !"
+            );
+            return $this->redirectToRoute('account_login'); 
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
